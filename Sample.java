b45a5e6ec6895d14298fa7fcc39bf9eac68fe909
4apple <4apple@163.com>
2017-11-02 09:37:14 +0800

    how-to-set-delay-in-android

--- Sample.java
+++ Sample.java
@@ -16,3 +16,15 @@
 * get method name
 */
 String name = new Object(){}.getClass().getEnclosingMethod().getName();
+
+/**
+*https://stackoverflow.com/questions/15874117/how-to-set-delay-in-android
+*/
+final Handler handler = new Handler();
+handler.postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        // Do something after 5s = 5000ms
+        buttons[inew][jnew].setBackgroundColor(Color.BLACK);
+    }
+}, 5000);
